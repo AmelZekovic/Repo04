8d254a559d182522dc410e54249f8bf6c94e08e7
AmelZekovic <amelzekovic@gmail.com>
2021-09-16 11:47:16 +0200

    Decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+
+    var myDecimal = 5.4;
+    var myDecimalE = 0.00254;
+    
+    return {
+        myDecimal,
+        myDecimalE
+    }
+}
+
+console.log(main());
+module.exports = main;
