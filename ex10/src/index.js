b95259f9e01a6ac95380b69be506c0f55fe72176
AmelZekovic <amelzekovic@gmail.com>
2021-09-17 09:09:19 +0200

    String variables

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    var myFirstName = "Amel";
+    var myLastName = "Zekovic";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
